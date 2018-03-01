96d3f57c338e9f040d6e9902b0d2a16b3344f8f0
James McNeil <=>
2018-03-01 10:38:49 +0000

    modified initialconfig.sh

--- provision_scripts/initialConfig.sh
+++ provision_scripts/initialConfig.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 
-sudo yum update -y
+# sudo yum update -y
 
 CONSUL_VERSION=1.0.2
 wget https://releases.hashicorp.com/consul/${CONSUL_VERSION}/consul_${CONSUL_VERSION}_linux_amd64.zip
